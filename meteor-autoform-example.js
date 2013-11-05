9a35bd64d496df3c3c64181d51753a394a8668a9
Eric Dobbertin <eric@dairystatedesigns.com>
2013-11-05 01:13:09 -0600

    more tests

--- meteor-autoform-example.js
+++ meteor-autoform-example.js
@@ -1,351 +1,425 @@
 //Schema
 Documents = new Meteor.Collection2("documents", {
-    schema: {
-        requiredString: {
-            type: String
-        },
-        optionalString: {
-            type: String,
-            optional: true
-        },
-        labelString: {
-            type: String,
-            label: "This is a custom label",
-            optional: true
-        },
-        longString: {
-            type: String,
-            optional: true,
-            max: 1000
-        },
-        maxString: {
-            type: String,
-            optional: true,
-            max: 10
-        },
-        minString: {
-            type: String,
-            optional: true,
-            min: 10
-        },
-        requiredBoolean: {
-            type: Boolean
-        },
-        optionalBoolean: {
-            type: Boolean,
-            optional: true
-        },
-        radioBoolean: {
-            type: Boolean,
-            optional: true
-        },
-        selectBoolean: {
-            type: Boolean,
-            optional: true
-        },
-        requiredSelect: {
-            type: Number,
-            allowedValues: [1, 2, 3]
-        },
-        optionalSelect: {
-            type: String,
-            allowedValues: ["One", "Two", "Three"],
-            optional: true
-        },
-        firstOptionSelect: {
-            type: Number,
-            allowedValues: [1, 2, 3],
-            optional: true
-        },
-        firstOptionRequiredSelect: {
-            type: Number,
-            valueIsAllowed: function(val) {
-                return (val === 1 || val === 2 || val === 3);
-            }
-        },
-        maxSelect: {
-            type: [Number],
-            allowedValues: [1, 2, 3],
-            optional: true,
-            maxCount: 2
-        },
-        minSelect: {
-            type: [Number],
-            allowedValues: [1, 2, 3],
-            optional: true,
-            minCount: 2
-        },
-        requiredNumber: {
-            type: Number
-        },
-        optionalNumber: {
-            type: Number,
-            optional: true
-        },
-        maxNumber: {
-            type: Number,
-            optional: true,
-            max: 10
-        },
-        minNumber: {
-            type: Number,
-            optional: true,
-            min: 10
-        },
-        decimal: {
-            type: Number,
-            optional: true,
-            decimal: true
-        },
-        requiredDate: {
-            type: Date
-        },
-        optionalDate: {
-            type: Date,
-            optional: true
-        },
-        minDate: {
-            type: Date,
-            optional: true,
-            min: (new Date(Date.UTC(2013, 0, 1)))
-        },
-        maxDate: {
-            type: Date,
-            optional: true,
-            max: (new Date(Date.UTC(2012, 11, 31)))
-        },
-        requiredEmail: {
-            type: String,
-            regEx: SchemaRegEx.Email,
-        },
-        optionalEmail: {
-            type: String,
-            regEx: SchemaRegEx.Email,
-            optional: true
-        },
-        requiredUrl: {
-            type: String,
-            regEx: SchemaRegEx.Url,
-        },
-        optionalUrl: {
-            type: String,
-            regEx: SchemaRegEx.Url,
-            optional: true
-        },
-        'a.b.c': {
-            type: String,
-            label: "Subdocument field value",
-            optional: true
-        }
+  schema: {
+    requiredString: {
+      type: String
+    },
+    optionalString: {
+      type: String,
+      optional: true
+    },
+    labelString: {
+      type: String,
+      label: "This is a custom label",
+      optional: true
+    },
+    longString: {
+      type: String,
+      optional: true,
+      max: 1000
+    },
+    maxString: {
+      type: String,
+      optional: true,
+      max: 10
+    },
+    minString: {
+      type: String,
+      optional: true,
+      min: 10
+    },
+    requiredBoolean: {
+      type: Boolean
+    },
+    optionalBoolean: {
+      type: Boolean,
+      optional: true
+    },
+    radioBoolean: {
+      type: Boolean,
+      optional: true
+    },
+    selectBoolean: {
+      type: Boolean,
+      optional: true
+    },
+    requiredSelect: {
+      type: Number,
+      allowedValues: [1, 2, 3]
+    },
+    optionalSelect: {
+      type: String,
+      allowedValues: ["One", "Two", "Three"],
+      optional: true
+    },
+    firstOptionSelect: {
+      type: Number,
+      allowedValues: [1, 2, 3],
+      optional: true
+    },
+    firstOptionRequiredSelect: {
+      type: Number,
+      valueIsAllowed: function(val) {
+        return (val === 1 || val === 2 || val === 3);
+      }
+    },
+    maxSelect: {
+      type: [Number],
+      allowedValues: [1, 2, 3],
+      optional: true,
+      maxCount: 2
+    },
+    minSelect: {
+      type: [Number],
+      allowedValues: [1, 2, 3],
+      optional: true,
+      minCount: 2
+    },
+    requiredNumber: {
+      type: Number
+    },
+    optionalNumber: {
+      type: Number,
+      optional: true
+    },
+    maxNumber: {
+      type: Number,
+      optional: true,
+      max: 10
+    },
+    minNumber: {
+      type: Number,
+      optional: true,
+      min: 10
+    },
+    decimal: {
+      type: Number,
+      optional: true,
+      decimal: true
+    },
+    requiredDate: {
+      type: Date
+    },
+    optionalDate: {
+      type: Date,
+      optional: true
+    },
+    minDate: {
+      type: Date,
+      optional: true,
+      min: (new Date(Date.UTC(2013, 0, 1)))
+    },
+    maxDate: {
+      type: Date,
+      optional: true,
+      max: (new Date(Date.UTC(2012, 11, 31)))
+    },
+    requiredEmail: {
+      type: String,
+      regEx: SchemaRegEx.Email
+    },
+    optionalEmail: {
+      type: String,
+      regEx: SchemaRegEx.Email,
+      optional: true
+    },
+    requiredUrl: {
+      type: String,
+      regEx: SchemaRegEx.Url
+    },
+    optionalUrl: {
+      type: String,
+      regEx: SchemaRegEx.Url,
+      optional: true
+    },
+    'a.b.c': {
+      type: String,
+      label: "Subdocument field value",
+      optional: true
+    },
+    arrayOfObjects: {
+      type: [Object],
+      optional: true
+    },
+    'arrayOfObjects.$.name': {
+      type: String,
+      label: "Array of object name"
     }
+  }
 });
 
 Documents.allow({
-    insert: function() {
-        return true;
-    },
-    update: function() {
-        return true;
-    },
-    remove: function() {
-        return true;
-    },
-    fetch: []
+  insert: function() {
+    return true;
+  },
+  update: function() {
+    return true;
+  },
+  remove: function() {
+    return true;
+  },
+  fetch: []
 });
 
 Documents.simpleSchema().messages({
-    noD: "No Ds allowed!!!",
-    'regEx requiredEmail': "[label] is not a valid e-mail address",
-    'regEx optionalEmail': "[label] is not a valid e-mail address",
-    'regEx requiredUrl': "[label] is not a valid URL",
-    'regEx optionalUrl': "[label] is not a valid URL"
+  noD: "No Ds allowed!!!",
+  'regEx requiredEmail': "[label] is not a valid e-mail address",
+  'regEx optionalEmail': "[label] is not a valid e-mail address",
+  'regEx requiredUrl': "[label] is not a valid URL",
+  'regEx optionalUrl': "[label] is not a valid URL"
 });
 
 Persons = new Meteor.Collection2("persons", {
-    schema: {
-        _id: {
-            type: String,
-            optional: true
-        },
-        firstName: {
-            type: String,
-            label: "First name",
-            max: 30
-        },
-        lastName: {
-            type: String,
-            label: "Last name",
-            max: 30,
-            unique: true
-        }
+  schema: {
+    _id: {
+      type: String,
+      optional: true
     },
-    virtualFields: {
-        fullName: function(person) {
-            return person.firstName + " " + person.lastName;
-        }
+    firstName: {
+      type: String,
+      label: "First name",
+      max: 30
+    },
+    lastName: {
+      type: String,
+      label: "Last name",
+      max: 30,
+      unique: true
     }
+  },
+  virtualFields: {
+    fullName: function(person) {
+      return person.firstName + " " + person.lastName;
+    }
+  }
 });
 
 Persons.allow({
-    insert: function() {
-        return true;
-    },
-    update: function() {
-        return true;
-    },
-    remove: function() {
-        return true;
-    },
-    fetch: []
+  insert: function() {
+    return true;
+  },
+  update: function() {
+    return true;
+  },
+  remove: function() {
+    return true;
+  },
+  fetch: []
 });
 
 Persons.simpleSchema().messages({
-    notUnique: "Only one of each last name allowed"
+  notUnique: "Only one of each last name allowed"
 });
 
 ContactForm = new AutoForm({
-    name: {
-        type: String,
-        label: "Your name",
-        max: 50
-    },
-    email: {
-        type: String,
-        regEx: SchemaRegEx.Email,
-        label: "E-mail address"
-    },
-    message: {
-        type: String,
-        label: "Message",
-        max: 1000
-    }
+  name: {
+    type: String,
+    label: "Your name",
+    max: 50
+  },
+  email: {
+    type: String,
+    regEx: SchemaRegEx.Email,
+    label: "E-mail address"
+  },
+  message: {
+    type: String,
+    label: "Message",
+    max: 1000
+  }
 });
 
 ContactForm.simpleSchema().messages({
-    'regEx email': "[label] is not a valid e-mail address"
+  'regEx email': "[label] is not a valid e-mail address"
+});
+
+Dates = new Meteor.Collection2("dates", {
+  schema: {
+    date: {
+      type: Date,
+      optional: true
+    },
+    dateTime: {
+      type: Date,
+      optional: true
+    },
+    dateTimeLocal: {
+      type: Date,
+      optional: true
+    }
+  }
+});
+
+Dates.allow({
+  insert: function() {
+    return true;
+  },
+  update: function() {
+    return true;
+  },
+  remove: function() {
+    return true;
+  },
+  fetch: []
 });
 
 if (Meteor.isClient) {
-    Meteor.subscribe("docs");
-    Meteor.subscribe("persons");
+  Meteor.subscribe("docs");
+  Meteor.subscribe("persons");
+  Meteor.subscribe("dates");
 
-    Documents.callbacks({
-        insert: function(error, result) {
-            if (error) {
-                console.log("Insert Error:", error);
-            } else {
-                alert("Inserted!");
-                console.log("Insert Result:", result);
-            }
-        },
-        update: function(error) {
-            if (error) {
-                console.log("Update Error:", error);
-            } else {
-                alert("Updated!");
-            }
-        },
-        remove: function(error) {
-            console.log("Remove Error:", error);
+  var cb = {
+    insert: function(error, result) {
+      if (error) {
+        console.log("Insert Error:", error);
+      } else {
+        console.log("Insert Result:", result);
+      }
+    },
+    update: function(error) {
+      if (error) {
+        console.log("Update Error:", error);
+      } else {
+        console.log("Updated!");
+      }
+    },
+    remove: function(error) {
+      console.log("Remove Error:", error);
+    }
+  };
+
+  Meteor.startup(function() {
+    Documents.callbacks(cb);
+    Dates.callbacks({
+      insert: function(error, result) {
+        if (error) {
+          console.log("Insert Error:", error);
+          console.log(Dates.namedContext("default").invalidKeys());
+        } else {
+          console.log("Inserted:", Dates.findOne(result));
         }
+      }
     });
-
-    Documents.simpleSchema().validator(function(key, val, def) {
-        if (val === "d") {
-            return "noD";
-        }
-        return true;
+    
+    Deps.autorun(function () {
+      var ctx = Documents.namedContext("docForm");
+      if (!ctx.isValid()) {
+        console.log(ctx.invalidKeys());
+      }
     });
+  });
 
-    Persons.beforeRemove = function(id) {
-        var name = Persons.findOne(id).fullName;
-        return confirm("Remove " + name + "?");
+  Documents.simpleSchema().validator(function(key, val, def) {
+    if (val === "d") {
+      return "noD";
+    }
+    return true;
+  });
+
+  Persons.beforeRemove = function(id) {
+    var name = Persons.findOne(id).fullName;
+    return confirm("Remove " + name + "?");
+  };
+
+  ContactForm.callbacks({
+    "sendEmail": function() {
+      console.log(_.toArray(arguments));
+    }
+  });
+
+  Template.contactForm.onSubmit = function() {
+    return function() {
+      console.log(_.toArray(arguments));
+      this.resetForm();
+      //return false;
     };
+  };
 
-    ContactForm.callbacks({
-        "sendEmail": function() {
-            console.log(_.toArray(arguments));
-        }
-    });
+  Template.example.schema = function() {
+    return Documents;
+  };
 
-    Template.contactForm.onSubmit = function() {
-        return function() {
-            console.log(_.toArray(arguments));
-            this.resetForm();
-            //return false;
-        };
-    };
+  Template.example.selectedDoc = function() {
+    return Documents.findOne(Session.get("selectedDoc"));
+  };
 
-    Template.example.schema = function() {
-        return Documents;
-    };
+  Template.datesForm.dateDoc = function() {
+    return Dates.findOne({_id: "pR95N5sxMTsaXe8e8"});
+  };
 
-    Template.example.selectedDoc = function() {
-        return Documents.findOne(Session.get("selectedDoc"));
-    };
+  Template.datesForm.dateTimeDoc = function() {
+    return Dates.findOne({_id: "TfFqjdJ6EYfjAdhE3"});
+  };
 
-    Template.docTable.documents = function() {
-        return Documents.find();
-    };
+  Template.datesForm.dateTimeLocalDoc = function() {
+    return Dates.findOne({_id: "pMcpm4vxkA5Hq4SyM"});
+  };
 
-    Template.buttons.newDocMode = function() {
-        return !Session.get("selectedDoc");
-    };
+  Template.docTable.documents = function() {
+    return Documents.find();
+  };
 
-    Template.example.events({
-        'click .docSelect': function(e, t) {
-            e.preventDefault();
-            Documents.resetForm("docForm");
-            Session.set("selectedDoc", this._id);
-        },
-        'click .docClear': function(e, t) {
-            e.preventDefault();
-            Documents.resetForm("docForm");
-            Session.set("selectedDoc", null);
-        }
-    });
+  Template.buttons.newDocMode = function() {
+    return !Session.get("selectedDoc");
+  };
 
-    Template.virtualFields.persons = function() {
-        return Persons.find();
-    };
+  Template.example.events({
+    'click .docSelect': function(e, t) {
+      e.preventDefault();
+      Documents.resetForm("docForm");
+      Session.set("selectedDoc", this._id);
+    },
+    'click .docClear': function(e, t) {
+      e.preventDefault();
+      Documents.resetForm("docForm");
+      Session.set("selectedDoc", null);
+    }
+  });
 
-    Handlebars.registerHelper("numSelectOptions", function() {
-        return [
-            {label: "One", value: 1},
-            {label: "Two", value: 2},
-            {label: "Three", value: 3}
-        ];
-    });
+  Template.virtualFields.persons = function() {
+    return Persons.find();
+  };
 
-    Handlebars.registerHelper("strSelectOptions", function() {
-        return [
-            {label: "One", value: "One"},
-            {label: "Two", value: "Two"},
-            {label: "Three", value: "Three"}
-        ];
-    });
+  Handlebars.registerHelper("numSelectOptions", function(options) {
+    return [
+      {label: "One", value: 1},
+      {label: "Two", value: 2},
+      {label: "Three", value: 3}
+    ];
+  });
+
+  Handlebars.registerHelper("strSelectOptions", function() {
+    return [
+      {label: "One", value: "One"},
+      {label: "Two", value: "Two"},
+      {label: "Three", value: "Three"}
+    ];
+  });
 }
 
 if (Meteor.isServer) {
-    Meteor.publish("docs", function() {
-        return Documents.find();
-    });
-    Meteor.publish("persons", function() {
-        return Persons.find();
-    });
+  Meteor.publish("docs", function() {
+    return Documents.find();
+  });
+  Meteor.publish("persons", function() {
+    return Persons.find();
+  });
+  Meteor.publish("dates", function() {
+    return Dates.find();
+  });
 
-    Meteor.methods({
-        sendEmail: function(doc) {
-            var text = "Name: " + doc.name + "\n\n"
-                    + "Email: " + doc.email + "\n\n\n\n"
-                    + doc.message;
+  Meteor.methods({
+    sendEmail: function(doc) {
+      var text = "Name: " + doc.name + "\n\n"
+              + "Email: " + doc.email + "\n\n\n\n"
+              + doc.message;
 
-            console.log("Sent E-mail:\n\n" + text);
-            return true;
-        }
-    });
+      console.log("Sent E-mail:\n\n" + text);
+      return true;
+    }
+  });
 
-    Meteor.startup(function() {
-        //Documents.remove({});
-    });
+  Meteor.startup(function() {
+    //Documents.remove({});
+  });
 }
